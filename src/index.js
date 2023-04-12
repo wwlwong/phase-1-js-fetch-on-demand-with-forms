4f3a4ac4c026a00143552ee7a6f2dd086dddb325
William Wong <william.wai.lim.wong@gmail.com>
2023-04-11 23:59:17 -0400

    js fetch on demand with forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form");
+
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        //console.log(event);
+        const input = document.querySelector("input#searchByID");
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then((response) => response.json())
+            .then((data) => {
+                const title = document.querySelector("section#movieDetails h4");
+                const summary = document.querySelector("section#movieDetails p");
+
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+        // LOG: (3) [{…}, {…}, {…}]
+            });
+    });
+};
 
 document.addEventListener('DOMContentLoaded', init);
